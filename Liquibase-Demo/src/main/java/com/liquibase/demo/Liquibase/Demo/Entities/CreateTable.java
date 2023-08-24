5a0fde4e1d88e54c5dfc43d20102bcd337ff6ca1
layakishore7 <layakishore4947@gmail.com>
2023-08-24 10:36:04 +0530

    commited

---
+++ Liquibase-Demo/src/main/java/com/liquibase/demo/Liquibase/Demo/Entities/CreateTable.java
@@ -0,0 +1,49 @@
+package com.liquibase.demo.Liquibase.Demo.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Account")
+public class CreateTable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id;
+
+	@Column
+	private String name;
+
+	@Column
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
---
+++ Liquibase-Demo/src/test/java/com/liquibase/demo/Liquibase/Demo/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquibase.demo.Liquibase.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
